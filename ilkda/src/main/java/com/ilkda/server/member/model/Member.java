a1e4fab89ac6c08c5d61124191e25f10cace5a01
황아영 <ayoganatanatda@naver.com>
2023-02-01 22:23:08 +0900

    Feat: Member Entity 추가

---
+++ ilkda/src/main/java/com/ilkda/server/member/model/Member.java
@@ -0,0 +1,31 @@
+package com.ilkda.server.member.model;
+
+import com.ilkda.server.base.BaseEntity;
+import lombok.AccessLevel;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+
+@Entity
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@Getter
+public class Member extends BaseEntity {
+
+    @Id @GeneratedValue
+    private Long id;
+
+    private Long kakaoId;
+
+    private String nickname;
+
+    private String profileImage;
+
+    @Builder
+    public Member(Long id) {
+        this.id = id;
+    }
+}
