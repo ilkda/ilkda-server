b983581abebfafd15927d338f320b9cae75e8532
황아영 <ayoganatanatda@naver.com>
2023-02-01 21:08:31 +0900

    Establish: 프로젝트 초기 설정

---
+++ ilkda/src/main/java/com/ilkda/server/IlkdaApplication.java
@@ -0,0 +1,13 @@
+package com.ilkda.server;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IlkdaApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(IlkdaApplication.class, args);
+    }
+
+}
