b89b19d693b2d977c8ed858c2890d2dc30bc5b63
황아영 <ayoganatanatda@naver.com>
2023-02-12 23:36:21 +0900

    Feat: ApiUtil 추가

---
+++ ilkda/src/main/java/com/ilkda/server/utils/api/ApiUtil.java
@@ -0,0 +1,21 @@
+package com.ilkda.server.utils.api;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+
+public class ApiUtil {
+
+    @Data
+    @AllArgsConstructor
+    public static class SuccessResponse<T> {
+        private final T response;
+    }
+
+    @Data
+    @AllArgsConstructor
+    public static class ErrorResponse {
+        private final int code;
+        private final String message;
+    }
+
+}
