fa3351079a1a19469d300258769e628c83456d99
oroko <67043922+dkdud9261@users.noreply.github.com>
2023-10-09 10:32:20 +0000

    Update IlkdaApplicationTests.java

--- ilkda/src/test/java/com/ilkda/server/IlkdaApplicationTests.java
+++ ilkda/src/test/java/com/ilkda/server/IlkdaApplicationTests.java
@@ -6,8 +6,8 @@ import org.springframework.boot.test.context.SpringBootTest;
 @SpringBootTest
 class IlkdaApplicationTests {
 
-    @Test
-    void contextLoads() {
-    }
+    // @Test
+    // void contextLoads() {
+    // }
 
 }
